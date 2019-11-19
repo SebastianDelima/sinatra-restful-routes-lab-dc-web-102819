564d4c700a83b7057d6a7dd1116ad9a0c368fd76
Sebastian Delima <sebastiandelima@Cactus1.local>
2019-11-19 17:05:04 -0500

    11examples_1failure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,35 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
   get '/recipes' do
-   recipe = Recipe.new
-    
-   end 
-  
+    @all_recipes = Recipe.all
+    erb :index
+  end
+   
+  get '/recipes/new' do
+    erb :new
+  end
 
+  post '/recipes'do
+  @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+  redirect to "/recipes/#{@recipe.id}"
+end
+
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  delete '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.delete
+    redirect to '/recipes'
+  end
+  
+  get '/recipes/:id/edit' do
+    @recipe = 
+   erb: edit
+  end
 
 end
---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+# Recipe.destroy_all
+# tomato_pie  = Recipe.create(:name => "Tomato pie", :ingredients => "Tomato", :cook_time => "30 minutes")
+# tomato_pie1  = Recipe.create(name: "Apple Pie", ingredients: "Apple", cook_time: "60 minutes")
+# tomato_pie  = Recipe.new(name: "", ingredients: "Tomato", cook_time: "30 minutes")
+# tomato_pie  = Recipe.new(name: "", ingredients: "Tomato", cook_time: "30 minutes")
+# tomato_pie  = Recipe.new(name: "", ingredients: "Tomato", cook_time: "30 minutes")
+# tomato_pie  = Recipe.new(name: "", ingredients: "Tomato", cook_time: "30 minutes")
